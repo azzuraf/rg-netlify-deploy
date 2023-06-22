6c0af69c2a3a75669073e69a67482705b8b5b806
Azzura Ferliani Ramadhani <52819640+azzuraf@users.noreply.github.com>
2023-06-22 11:31:35 +0700

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://bright-ganache-b7c518.netlify.app" // TODO: replace this
+const studentName = "Azzura Ferliani Ramadhani" // TODO: replace this
+const studentId = "FE4863101" // TODO: replace this
+
+export { url, studentName, studentId }
